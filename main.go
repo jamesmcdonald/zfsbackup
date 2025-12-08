dd73e68c3524bc8736a267c2253259875f63fe04
James McDonald <james@jamesmcdonald.com>
2025-12-08 12:43:47 +0100

    Enable debug logging

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/jamesmcdonald/zfsbackup/zfs"
 )
 
 func main() {
+	slog.SetLogLoggerLevel(slog.LevelDebug)
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage: %s <volume>\n", os.Args[0])
+		os.Exit(1)
+	}
+	vol := os.Args[1]
 	b := zfs.NewBackup("backup")
-	err := b.IncrementalBackup("chonk")
+	err := b.IncrementalBackup(vol)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error during backup:", err)
 	}
