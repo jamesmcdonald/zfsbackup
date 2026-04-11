7029fb4857af6c29437b824ac38b44d6e36fb0b7
James McDonald <james@jamesmcdonald.com>
2026-04-11 12:50:09 +0200

    Add rate to bar

--- progress/bar.go
+++ progress/bar.go
@@ -4,8 +4,19 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sync"
 	"sync/atomic"
+	"time"
+
+	"github.com/jamesmcdonald/zfsbackup/zfs"
 )
+
+const maxSamples = 60
+
+type sample struct {
+	t time.Time
+	n int64
+}
 
 type Bar struct {
 	label    string
@@ -13,6 +24,11 @@ type Bar struct {
 	pos      atomic.Int64
 	done     atomic.Bool
 	abbrLeft bool
+
+	mu          sync.Mutex
+	samples     [maxSamples]sample
+	sampleIdx   int
+	sampleCount int
 }
 
 func NewBar(label string, target int64) *Bar {
@@ -42,19 +58,56 @@ func (b *Bar) displayLabel(width int) string {
 	return b.label
 }
 
+func (b *Bar) recordSample(n int64) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	b.samples[b.sampleIdx] = sample{t: time.Now(), n: n}
+	b.sampleIdx = (b.sampleIdx + 1) % maxSamples
+	if b.sampleCount < maxSamples {
+		b.sampleCount++
+	}
+}
+
+func (b *Bar) rate() float64 {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	if b.sampleCount < 2 {
+		return 0
+	}
+	oldest := (b.sampleIdx - b.sampleCount + maxSamples) % maxSamples
+	newest := (b.sampleIdx - 1 + maxSamples) % maxSamples
+	dt := b.samples[newest].t.Sub(b.samples[oldest].t).Seconds()
+	if dt <= 0 {
+		return 0
+	}
+	return float64(b.samples[newest].n-b.samples[oldest].n) / dt
+}
+
 func (b *Bar) draw(output *os.File, width int) {
-	pct := float64(b.pos.Load()) / float64(b.target)
-	filled := int(pct * float64(width-30))
+	pos := b.pos.Load()
+	b.recordSample(pos)
+
+	pct := float64(pos) / float64(b.target)
+
+	sizeText := fmt.Sprintf("%s / %s", zfs.HumanBytes(pos), zfs.HumanBytes(b.target))
+	rateText := fmt.Sprintf("%s/s", zfs.HumanBytes(int64(b.rate())))
+
+	// 20 (label) + 2 ( [) + 2 (] ) + 4 (pct) + 2 (  ) + sizeText + 2 (  ) + rateText
+	overhead := 20 + 2 + 2 + 4 + 2 + len(sizeText) + 2 + len(rateText)
+	barWidth := max(width-overhead, 5)
+
+	filled := int(pct * float64(barWidth))
 	fillChar := "="
-	if filled > width-30 {
-		filled = width - 30
+	if filled > barWidth {
+		filled = barWidth
 		fillChar = ">"
 	}
+
 	dl := b.displayLabel(20)
-	// Kill to EOL after printing
 	pctText := "Done"
 	if !b.done.Load() {
 		pctText = fmt.Sprintf("%3.0f%%", pct*100)
 	}
-	fmt.Fprintf(output, "%20s [%-*s] %s\033[0K\n", dl, width-30, strings.Repeat(fillChar, filled), pctText)
+	// Kill to EOL after printing
+	fmt.Fprintf(output, "%20s [%-*s] %s  %s  %s\033[0K\n", dl, barWidth, strings.Repeat(fillChar, filled), pctText, sizeText, rateText)
 }
